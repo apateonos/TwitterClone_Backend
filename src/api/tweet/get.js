7a7873006e2c149f0d763e7756a8afc0c5902163
GyuYoung Kim <apateonos@gmail.com>
2021-04-28 06:30:07 +0900

    d

--- src/api/tweet/get.js
+++ src/api/tweet/get.js
@@ -5,10 +5,11 @@ export default async (req, res, next) => {
   try {
     const { tweet_id, unique_name } = req.query;
     const user_id = req.user_id;
+    
     if (tweet_id) {
-      console.log(tweet_id);
       const value = [ tweet_id ];
       const [ tweets ] = await database.query(SELECT_TWEET, value);
+      res.data = { ...res.data, tweets }; 
     } 
     if (unique_name) {
       const value = [ unique_name, unique_name ];
