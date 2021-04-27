92d3fce2c062ea7b079f86fc2105927810520c86
GyuYoung Kim <apateonos@gmail.com>
2021-04-28 06:27:18 +0900

    d

--- src/api/reply/get.js
+++ src/api/reply/get.js
@@ -4,7 +4,6 @@ import { SELECT_TWEET_REPLYS } from '../../db/query';
 export default async (req, res, next) => {
   try {
     const { tweet_id } = req.query;
-    if (typeof tweet_id !== 'number') throw {code: 'ER_INVAILD_TARGET', message: 'invaild request'};
 
     const value = [ tweet_id ];
     const [ replys ] = await database.query(SELECT_TWEET_REPLYS, value);
