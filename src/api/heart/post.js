eabef94d6f8d65740252ff9a3c761d14c0ee7e11
GyuYoung Kim <apateonos@gmail.com>
2021-04-28 09:37:00 +0900

    d

--- src/api/heart/post.js
+++ src/api/heart/post.js
@@ -5,11 +5,10 @@ export default async (req, res, next) => {
   try {
     const { tweet_id } = req.body;
     const user_id = req.user_id;
-    //if (typeof tweet_id !== 'number') throw {code: 'ER_INVAILD_TARGET', message: 'invaild request'};
-
     const value = [ user_id, tweet_id ];
-    const [ checkDup ] = await database.query(CHECK_HEART_DUP, value);
-    if (checkDup.length > 0) throw {code: 'ER_DUP_HEART', message: 'dupicated heart'};
+
+    const [ check ] = await database.query(CHECK_HEART_DUP, value);
+    if (check.length > 0) throw {code: 'ER_DUP_HEART', message: 'dupicated heart'};
     const [ result ] = await database.query(INSERT_HEART, value);
 
     res.data = { ...res.data, result };
