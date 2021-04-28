5b72545f72a7137f18b89512abf5914f2c4981ac
GyuYoung Kim <apateonos@gmail.com>
2021-04-28 10:22:45 +0900

    d

--- src/api/reply/get.js
+++ src/api/reply/get.js
@@ -4,10 +4,11 @@ import { SELECT_TWEET_REPLYS } from '../../db/query';
 export default async (req, res, next) => {
   try {
     const { tweet_id } = req.query;
-
+    console.log('>>>>>' tweet_id);
     const value = [ tweet_id ];
+    console.log(value, SELECT_TWEET_REPLYS);
     const [ replys ] = await database.query(SELECT_TWEET_REPLYS, value);
-
+    console.log(replys);
     res.data = { ...res.data, replys };
     next(); 
   } catch (err) {
